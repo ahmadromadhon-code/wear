15f1ab8f7d334a0eec959dac83cbd25ec8348b1c
ancientmoon <155276042+ahmadromadhon-code@users.noreply.github.com>
2025-04-07 11:07:54 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
 const API_BASE_URL = location.hostname.includes('github.io')
-  ? 'https://wearbackend.vercel.app
-'
-  : '';
+  ? 'https://wearbackend.vercel.app'
+  : 'http://localhost:3000'; // atau base url lain untuk dev
